643e8e5718c31c2ef59387b221a7b9f7bb5a87ae
Regashia <regashia@gmail.com>
2018-08-29 12:09:36 +0900

    Add editorconfig-vim

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -15,4 +15,15 @@ source ~/.vim_runtime/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
+function install_plugin() {
+    local user_name=$(echo ${1} | cut -d '/' -f 1)
+    local repository_name=$(echo ${1} | cut -d '/' -f 2)
+    rm -rf ${repository_name}
+    git clone --depth=1 https://github.com/${1}.git
+}
+
+cd my_plugins
+
+install_plugin "editorconfig/editorconfig-vim"
+
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
